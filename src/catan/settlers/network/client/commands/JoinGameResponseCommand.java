7be18c604e0edee994b7e5fe6f5e98874fabe3d4
Simon Huang <simh@live.ca>
2017-03-01 15:25:48 -0500

    join game response

--- src/catan/settlers/network/client/commands/JoinGameResponseCommand.java
+++ src/catan/settlers/network/client/commands/JoinGameResponseCommand.java
@@ -1,5 +1,10 @@
 package catan.settlers.network.client.commands;
 
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import catan.settlers.client.view.setup.MainFrame;
+
 public class JoinGameResponseCommand implements ServerToClientCommand {
 	
 	private boolean success;
@@ -10,6 +15,16 @@ public class JoinGameResponseCommand implements ServerToClientCommand {
 	
 	@Override
 	public void execute() {
-		// TODO Handle Join game result here		
+		// TODO Handle Join game result here
+		if(success){
+		new WaitingRoom(ArrayList<Player> participants);
+		MainFrame.getInstance().remove(MainFrame.getInstance().getContentPane());
+		MainFrame.getInstance().add(WaitingRoom.getPanel());
+		MainFrame.getInstance().setContentPane(WaitingRoom.getPanel());
+		MainFrame.getInstance().revalidate();
+		MainFrame.getInstance().repaint();
+		}else{
+			JOptionPane.showMessageDialog(new JLabel(), "Room full");
+		}
 	}
 }
