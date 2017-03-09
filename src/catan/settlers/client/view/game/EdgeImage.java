da1b0da6e5313a4b68e930353006feb68ac88bad
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-03-09 11:36:40 -0500

    Second click on edge deselects it

--- src/catan/settlers/client/view/game/EdgeImage.java
+++ src/catan/settlers/client/view/game/EdgeImage.java
@@ -14,29 +14,28 @@ public class EdgeImage extends MinuetoRectangle implements Clickable {
 	private int relativeX;
 	private int relativeY;
 	private Edge edgeModel;
-	
+
 	private int rot_width;
 	private int rot_height;
 	private MinuetoColor edgeMColor;
-	
-		
+
+	private boolean selected;
 
 	public EdgeImage(Edge edge, double rotation, int relativeX, int relativeY, MinuetoColor edgeMColor) {
 		super(40, 10, edgeMColor, true);
-		
+
 		MinuetoImage rotated = rotate(rotation);
 		this.rot_width = rotated.getWidth();
-		this.rot_height = rotated.getHeight();	
+		this.rot_height = rotated.getHeight();
 		this.edgeModel = edge;
 
 		this.relativeX = relativeX;
 		this.relativeY = relativeY;
-				
-		
+
+		this.selected = false;
+
 	}
 
-	
-	
 	@Override
 	public boolean isClicked(int x, int y) {
 		return x > relativeX && x < relativeX + getWidth() && y > relativeY + 100 && y < relativeY + 100 + getHeight();
@@ -44,19 +43,25 @@ public class EdgeImage extends MinuetoRectangle implements Clickable {
 
 	@Override
 	public void onclick() {
-		ClientModel.instance.setCurrentEdge(edgeModel);
+		selected = !selected;
+
+		if (selected) {
+			ClientModel.instance.setCurrentEdge(edgeModel);
+		} else {
+			ClientModel.instance.setCurrentEdge(null);
+		}
 	}
 
 	@Override
 	public String getName() {
 		return "Edge";
 	}
-	
+
 	@Override
 	public int getWidth() {
 		return rot_width;
 	}
-	
+
 	@Override
 	public int getHeight() {
 		return rot_height;
