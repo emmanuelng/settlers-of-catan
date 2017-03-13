e4d436490889e8c10b8788a451a9d4d7af049c39
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-03-12 22:59:00 -0400

    Functional Setup phase one

--- src/catan/settlers/network/client/commands/game/PlaceElmtsSetupPhaseCommand.java
+++ src/catan/settlers/network/client/commands/game/PlaceElmtsSetupPhaseCommand.java
@@ -5,15 +5,25 @@ import catan.settlers.client.view.game.DialogBox;
 import catan.settlers.client.view.game.GameWindow;
 import catan.settlers.network.client.commands.ServerToClientCommand;
 
-public class PlaceElmtsSetupPhaseOneCommand implements ServerToClientCommand {
+public class PlaceElmtsSetupPhaseCommand implements ServerToClientCommand {
 
 	private static final long serialVersionUID = 1L;
+	private boolean isPhaseOne;
+
+	public PlaceElmtsSetupPhaseCommand(boolean isPhaseOne) {
+		this.isPhaseOne = isPhaseOne;
+	}
 
 	@Override
 	public void execute() {
 		GameWindow window = ClientWindow.getInstance().getGameWindow();
 		DialogBox dbox = new DialogBox("Place your first settlement and road",
 				"Select an edge and an intersection and click on \"End Turn\" to confirm");
+
+		if (!isPhaseOne) {
+			dbox = new DialogBox("Place your second settlement and road",
+					"Select an edge and an intersection and click on \"End Turn\" to confirm");
+		}
 		window.setDialogBox(dbox);
 	}
 }
--- src/catan/settlers/server/model/Game.java
+++ src/catan/settlers/server/model/Game.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 import catan.settlers.network.client.commands.TurnResponseCommand;
-import catan.settlers.network.client.commands.game.PlaceElmtsSetupPhaseOneCommand;
+import catan.settlers.network.client.commands.game.PlaceElmtsSetupPhaseCommand;
 import catan.settlers.network.client.commands.game.UpdateGameBoardCommand;
 import catan.settlers.network.client.commands.game.WaitForPlayerCommand;
 import catan.settlers.network.server.Server;
@@ -22,7 +22,7 @@ public class Game implements Serializable {
 		READYTOJOIN, SETUPPHASEONE, SETUPPHASETWO, TURNPHASEONE
 	}
 
-	public static final int MAX_NB_OF_PLAYERS = 1;
+	public static final int MAX_NB_OF_PLAYERS = 2;
 	private static final long serialVersionUID = 1L;
 
 	private int id;
@@ -31,8 +31,6 @@ public class Game implements Serializable {
 	private GameBoardManager gameBoardManager;
 	private Player currentPlayer;
 	private GamePhase currentPhase;
-
-	int phaseCounter;
 
 	public Game(int id, Player owner) {
 		this.id = id;
@@ -45,14 +43,11 @@ public class Game implements Serializable {
 
 	public void startGame() {
 		this.currentPhase = GamePhase.SETUPPHASEONE;
-		phaseCounter = 0;
-		// Send Place settlement command to current player, wait command to the
-		// others
-		currentPlayer = participants.get((int) Math.random() * participants.size());
+		this.currentPlayer = participants.get(0);
 
 		for (Player p : participants) {
 			if (p == currentPlayer) {
-				p.sendCommand(new PlaceElmtsSetupPhaseOneCommand());
+				p.sendCommand(new PlaceElmtsSetupPhaseCommand(true));
 			} else {
 				p.sendCommand(new WaitForPlayerCommand(currentPlayer.getUsername()));
 			}
@@ -63,10 +58,10 @@ public class Game implements Serializable {
 	public void receiveResponse(Player sender, TurnData data) {
 		switch (currentPhase) {
 		case SETUPPHASEONE:
-			setupPhaseOne(sender, data);
+			setupPhase(sender, data, true);
 			break;
 		case SETUPPHASETWO:
-			setupPhaseTwo(sender, data);
+			setupPhase(sender, data, false);
 			break;
 		default:
 			break;
@@ -76,85 +71,82 @@ public class Game implements Serializable {
 
 	/* ============ Game phases logic ============ */
 
-	private void setupPhaseOne(Player sender, TurnData data) {
+	private void setupPhase(Player sender, TurnData data, boolean isPhaseOne) {
 		if (sender == currentPlayer) {
+			// If no edge or intersection selected, reject
+			if (data.getEdgeSelection() == null || data.getIntersectionSelection() == null) {
+				currentPlayer.sendCommand(new TurnResponseCommand("Please select an edge and an intersection", false));
+				return;
+			}
+
+			// Get the selected intersection and edge
 			GameBoard board = gameBoardManager.getBoard();
 			Intersection interSelect = board.getIntersectionById(data.getIntersectionSelection().getId());
 			Edge edgeSelect = board.getEdgeById(data.getEdgeSelection().getId());
 
 			if (edgeSelect.hasIntersection(interSelect) && edgeSelect.getOwner() == null && interSelect.canBuild()) {
+				// Update the board and send it to all the players
 				Village v = new Village(currentPlayer);
 				interSelect.setUnit(v);
 				edgeSelect.setOwner(currentPlayer);
-				
-				System.out.println("Sending refresh commands");
-				for (Player p : participants) {
-					System.out.println("Command sent");
-					p.sendCommand(new UpdateGameBoardCommand(gameBoardManager.getBoardDeepCopy()));
+				updateAllPlayers();
+
+				if (!isPhaseOne) {
+					// In second setup phase, give the corresponding resources
+					// to the player
+					ArrayList<Hexagon> drawFor = interSelect.getHexagons();
+					for (Hexagon h : drawFor) {
+						ResourceType r = terrainToResource(h.getType());
+						if (r != null) {
+							currentPlayer.giveResource(r, 1);
+						}
+					}
 				}
 
-				currentPlayer.sendCommand(new TurnResponseCommand("You've placed a settlement and road!", true));
-
-				if (isLastPlayer(currentPlayer)) {
+				// Stopping condition for phase one
+				if (isPhaseOne && currentPlayer == participants.get(participants.size() - 1)) {
 					currentPhase = GamePhase.SETUPPHASETWO;
-					phaseCounter = 0;
-				} else {
+					for (Player p : participants) {
+						if (p == currentPlayer) {
+							p.sendCommand(new PlaceElmtsSetupPhaseCommand(false));
+						} else {
+							p.sendCommand(new WaitForPlayerCommand(currentPlayer.getUsername()));
+						}
+					}
+					return;
+				}
+				
+				// Get the next player
+				if (isPhaseOne) {
 					currentPlayer = nextPlayer();
-					for (Player p : participants) {
-						if (p == currentPlayer) {
-							p.sendCommand(new PlaceElmtsSetupPhaseOneCommand());
-						} else {
-							p.sendCommand(new WaitForPlayerCommand(currentPlayer.getUsername()));
-						}
-					}
-				}
-			} else {
-				currentPlayer.sendCommand(new TurnResponseCommand("Invalid selection. Try again.", false));
-			}
-		}
-	}
-
-	private void setupPhaseTwo(Player sender, TurnData data) {
-		if (sender == currentPlayer) {
-			Intersection interSelect = gameBoardManager.getBoard()
-					.getIntersectionById(data.getIntersectionSelection().getId());
-			Edge edgeSelect = gameBoardManager.getBoard().getEdgeById(data.getEdgeSelection().getId());
-			if (edgeSelect.hasIntersection(interSelect) && edgeSelect.getOwner() == null && interSelect.canBuild()) {
-				Village v = new Village(currentPlayer);
-				v.upgradeToCity();
-				interSelect.setUnit(v);
-				edgeSelect.setOwner(currentPlayer);
-
-				ArrayList<Hexagon> drawFor = interSelect.getHexagons();
-				for (Hexagon h : drawFor) {
-					ResourceType r = terrainToResource(h.getType());
-					if (r != null) {
-						currentPlayer.giveResource(r, 1);
-					}
-				}
-
-				UpdateGameBoardCommand refresh = new UpdateGameBoardCommand(gameBoardManager.getBoard());
-				for (Player p : participants) {
-					p.sendCommand(refresh);
-				}
-				currentPlayer.sendCommand(new TurnResponseCommand("You've placed a settlement and road!", true));
-
-				phaseCounter++;
-				if (phaseCounter == participants.size()) {
-					currentPhase = GamePhase.TURNPHASEONE;
-					phaseCounter = 0;
 				} else {
-					prevPlayer();
-					for (Player p : participants) {
-						if (p == currentPlayer) {
-							p.sendCommand(new PlaceElmtsSetupPhaseOneCommand());
-						} else {
-							p.sendCommand(new WaitForPlayerCommand(currentPlayer.getUsername()));
-						}
+					currentPlayer = previousPlayer();
+				}
+				
+				// Send commands to the players
+				for (Player p : participants) {
+					if (p == currentPlayer) {
+						p.sendCommand(new PlaceElmtsSetupPhaseCommand(isPhaseOne));
+					} else {
+						p.sendCommand(new WaitForPlayerCommand(currentPlayer.getUsername()));
 					}
 				}
+
+				// Stopping condition for phase two
+				if (!isPhaseOne && currentPlayer == participants.get(0)) {
+					currentPhase = GamePhase.TURNPHASEONE;
+				}
+
 			} else {
-				currentPlayer.sendCommand(new TurnResponseCommand("Invalid selection. Try again.", false));
+				String message = "";
+				if (!edgeSelect.hasIntersection(interSelect)) {
+					message = "The intersection and the edge must be adjacent";
+				} else if (edgeSelect.getOwner() != null) {
+					message = "You cannot build on an ocupied edge/intersection";
+				} else {
+					message = "You cannot build here";
+				}
+				currentPlayer.sendCommand(new TurnResponseCommand(message, false));
 			}
 		}
 	}
@@ -170,8 +162,12 @@ public class Game implements Serializable {
 		return participants.get(index);
 	}
 
-	public boolean isLastPlayer(Player p) {
-		return participants.indexOf(p) == participants.size() - 1;
+	private Player previousPlayer() {
+		int index = participants.indexOf(currentPlayer) - 1;
+		if (index < 0) {
+			index = participants.size() - 1;
+		}
+		return participants.get(index);
 	}
 
 	public void prevPlayer() {
@@ -191,7 +187,7 @@ public class Game implements Serializable {
 		Server.getInstance().getGameManager().removeGame(this);
 	}
 
-	private ResourceType terrainToResource(TerrainType t) {
+	public ResourceType terrainToResource(TerrainType t) {
 		switch (t) {
 		case FOREST:
 			return ResourceType.LUMBER;
@@ -205,6 +201,12 @@ public class Game implements Serializable {
 			return ResourceType.GRAIN;
 		default:
 			return null;
+		}
+	}
+
+	private void updateAllPlayers() {
+		for (Player p : participants) {
+			p.sendCommand(new UpdateGameBoardCommand(gameBoardManager.getBoardDeepCopy()));
 		}
 	}
 }
