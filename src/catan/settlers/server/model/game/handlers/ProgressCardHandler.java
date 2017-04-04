40fcba1a821f9fbf75c3a14839da7db8432a037c
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-04-04 11:25:03 -0400

    Moved different progress cards in separate methods

--- src/catan/settlers/server/model/game/handlers/ProgressCardHandler.java
+++ src/catan/settlers/server/model/game/handlers/ProgressCardHandler.java
@@ -24,155 +24,327 @@ public class ProgressCardHandler {
 		this.game = game;
 	}
 
+	/**
+	 * Progress card logic handling.
+	 * 
+	 * ----------------------------
+	 * TODO: Implement the TODO methods. Once this is done, remove the TODO tag.
+	 * You can go directly to the method using Ctrl + click on the method name.
+	 * You can also get the method description by hovering on the method
+	 * signature.
+	 * ----------------------------
+	 */
 	public void handle(Player sender, ProgressCardType card) {
 		switch (card) {
 		case COMMERCIAL_HARBOR:
-			// give each opponent a resource of your choice in exchange for a
-			// commodity of their choice
+			commercialHarbor(); // TODO
 			break;
 		case MASTER_MERCHANT:
-			// choose two cards to take from an opponent with more VPs than you
+			masterMerchant(sender); // TODO
 			break;
 		case MERCHANT:
-			// place merchant on a tile; trade that resource at 2:1
+			merchant(sender); // TODO
 			break;
 		case MERCHANT_FLEET:
-			// select a resource; you may trade that at 2:1 for this turn
+			merchantFleet(sender); // TODO
 			break;
 		case RESOURCE_MONOPOLY:
-			// name a resource; all other players must give you 2 of that if
-			// they have
+			resourceMonopoly(sender); // TODO
 			break;
 		case TRADE_MONOPOLY:
-			// name a commodity; all players must give you 1 if that if they
-			// have it
+			tradeMonopoly(sender); // TODO
 			break;
 		case BISHOP:
-			// move the robber, but get a random card from EACH player on the
-			// robber's new hex
+			bishop(sender); // TODO
 			break;
 		case CONSTITUTION:
-			// one VP for player
+			constitution(sender); // TODO
 			break;
 		case DESERTER:
-			// choose an opponent who must remove a knight; place a knight of
-			// equal strength
+			deserter(sender); // TODO
 			break;
 		case DIPLOMAT:
-			// remove any open road; if it's your own, you may place it
-			// somewhere else
+			diplomat(sender); // TODO
 			break;
 		case INTRIGUE:
-			// choose an opponent's knight to displace for free, but it must be
-			// on your road network
+			intrigue(sender); // TODO
 			break;
 		case SABOTEUR:
-			// all players with more VPs than you must discard half their cards
+			saboteur(sender); // TODO
 			break;
 		case SPY:
-			// steal a non-VP progress card from an opponent
+			spy(sender); // TODO
 			break;
 		case WARLORD:
-			// Activate all your knights for freeeeee
-			ArrayList<Intersection> intersections = game.getGameBoardManager().getBoard().getIntersections();
-			for (Intersection i : intersections) {
-				IntersectionUnit u = i.getUnit();
-				if (u instanceof Knight) {
-					Knight knight = (Knight) u;
-					if (knight.getOwner() == sender) {
-						knight.activateKnight();
-					}
-				}
-			}
+			warlord(sender);
 			break;
 		case WEDDING:
-			// All players with more VPs than you must give you 2 resources of
-			// their choice
+			wedding(sender); // TODO
 			break;
 		case CRANE:
-			// Your next city improvement you build this turn costs one less
-			// commodity than usual
-			sender.playCrane();
+			crane(sender);
 			break;
 		case ENGINEER:
-			// Your next city wall is free
-			sender.playEngineer();
+			engineer(sender);
 			break;
 		case INVENTOR:
-			// Swap the numbers of two hexagons on the board. They must not be
-			// 2, 6, 8, 12
+			inventor(sender); // TODO
 			break;
 		case IRRIGATION:
-			// Draw two grain for each field tile you have at least one village
-			// on
-			for (int y = 0; y < 7; y++) {
-				for (int x = 0; x < 7; x++) {
-					Hexagon h = game.getGameBoardManager().getBoard().getHexagonAt(x, y);
-					if (h != null) {
-						if (h.getType() == TerrainType.FIELD) {
-							boolean onHex = false;
-							for (IntersectionLoc loc : IntersectionLoc.values()) {
-								IntersectionUnit u = h.getIntersection(loc).getUnit();
-								if (u instanceof Village) {
-									if (u.getOwner() == sender) {
-										onHex = true;
-										break;
-									}
-								}
-							}
-							if (onHex) {
-								sender.giveResource(ResourceType.GRAIN, 2);
-								sender.sendCommand(new UpdateResourcesCommand(currentPlayer.getResources()));
-							}
-						}
-					}
-				}
-			}
+			irrigation(sender);
 			break;
 		case MEDICINE:
-			// Your next city upgrade this turn costs 2 ore and 1 grain
-			sender.playMedicine();
+			medicine(sender); // TODO
 			break;
 		case MINING:
-			// Draw two ore for each mountain tile you have at least one village
-			// on
-			for (int y = 0; y < 7; y++) {
-				for (int x = 0; x < 7; x++) {
-					Hexagon h = game.getGameBoardManager().getBoard().getHexagonAt(x, y);
-					if (h != null) {
-						if (h.getType() == TerrainType.MOUNTAIN) {
-							boolean onHex = false;
-							for (IntersectionLoc loc : IntersectionLoc.values()) {
-								IntersectionUnit u = h.getIntersection(loc).getUnit();
-								if (u instanceof Village) {
-									if (u.getOwner() == sender) {
-										onHex = true;
-										break;
-									}
-								}
-							}
-							if (onHex) {
-								sender.giveResource(ResourceType.ORE, 2);
-								sender.sendCommand(new UpdateResourcesCommand(currentPlayer.getResources()));
-							}
-						}
-					}
-				}
-			}
+			mining(sender);
 			break;
 		case PRINTER:
-			// Get one VP!
+			printer(sender); // TODO
 			break;
 		case ROAD_BUILDING:
-			// Build two roads at no cost
-			sender.playRoadBuilding();
+			roadBuilding(sender); // TODO
 			break;
 		case SMITH:
-			// Upgrade two knights at no cost
-			sender.playSmith();
+			smithCard(sender); // TODO
 			break;
 		default:
 			break;
 		}
+	}
+
+	/**
+	 * give each opponent a resource of your choice in exchange for a commodity
+	 * of their choice
+	 */
+	private void commercialHarbor() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * choose two cards to take from an opponent with more VPs than you
+	 */
+	private void masterMerchant(Player sender) {
+
+	}
+
+	/**
+	 * place merchant on a tile; trade that resource at 2:1
+	 * 
+	 * @param sender
+	 */
+	private void merchant(Player sender) {
+
+	}
+
+	/**
+	 * select a resource; you may trade that at 2:1 for this turn
+	 */
+	private void merchantFleet(Player sender) {
+
+	}
+
+	/**
+	 * name a resource; all other players must give you 2 of that if they have
+	 */
+	private void resourceMonopoly(Player sender) {
+
+	}
+
+	/**
+	 * name a commodity; all players must give you 1 if that if they have it
+	 */
+	private void tradeMonopoly(Player sender) {
+
+	}
+
+	/**
+	 * move the robber, but get a random card from EACH player on the robber's
+	 * new hex
+	 * 
+	 * @param sender
+	 */
+	private void bishop(Player sender) {
+
+	}
+
+	/**
+	 * one VP for player
+	 */
+	private void constitution(Player sender) {
+
+	}
+
+	/**
+	 * choose an opponent who must remove a knight; place a knight of equal
+	 * strength
+	 */
+	private void deserter(Player sender) {
+
+	}
+
+	/**
+	 * remove any open road; if it's your own, you may place it somewhere else
+	 */
+	private void diplomat(Player sender) {
+
+	}
+
+	/**
+	 * choose an opponent's knight to displace for free, but it must be on your
+	 * road network
+	 */
+	private void intrigue(Player sender) {
+
+	}
+
+	/**
+	 * all players with more VPs than you must discard half their cards
+	 * 
+	 * @param sender
+	 */
+	private void saboteur(Player sender) {
+
+	}
+
+	/**
+	 * steal a non-VP progress card from an opponent
+	 */
+	private void spy(Player sender) {
+
+	}
+
+	/**
+	 * Activate all your knights for free
+	 */
+	private void warlord(Player sender) {
+		ArrayList<Intersection> intersections = game.getGameBoardManager().getBoard().getIntersections();
+		for (Intersection i : intersections) {
+			IntersectionUnit u = i.getUnit();
+			if (u instanceof Knight) {
+				Knight knight = (Knight) u;
+				if (knight.getOwner() == sender) {
+					knight.activateKnight();
+				}
+			}
+		}
+	}
+
+	/**
+	 * All players with more VPs than you must give you 2 resources of their
+	 * choice
+	 */
+	private void wedding(Player sender) {
+
+	}
+
+	/**
+	 * Your next city improvement you build this turn costs one less commodity
+	 * than usual
+	 */
+	private void crane(Player sender) {
+		sender.playCrane();
+	}
+
+	/**
+	 * Your next city wall is free
+	 */
+	private void engineer(Player sender) {
+		sender.playEngineer();
+	}
+
+	/**
+	 * Swap the numbers of two hexagons on the board. They must not be 2, 6, 8,
+	 * 12
+	 */
+	private void inventor(Player sender) {
+
+	}
+
+	/**
+	 * Draw two grain for each field tile you have at least one village on
+	 */
+	private void irrigation(Player sender) {
+		for (int y = 0; y < 7; y++) {
+			for (int x = 0; x < 7; x++) {
+				Hexagon h = game.getGameBoardManager().getBoard().getHexagonAt(x, y);
+				if (h != null) {
+					if (h.getType() == TerrainType.FIELD) {
+						boolean onHex = false;
+						for (IntersectionLoc loc : IntersectionLoc.values()) {
+							IntersectionUnit u = h.getIntersection(loc).getUnit();
+							if (u instanceof Village) {
+								if (u.getOwner() == sender) {
+									onHex = true;
+									break;
+								}
+							}
+						}
+						if (onHex) {
+							sender.giveResource(ResourceType.GRAIN, 2);
+							sender.sendCommand(new UpdateResourcesCommand(currentPlayer.getResources()));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Your next city upgrade this turn costs 2 ore and 1 grain
+	 */
+	private void medicine(Player sender) {
+		sender.playMedicine();
+	}
+
+	/**
+	 * Draw two ore for each mountain tile you have at least one village on
+	 */
+	private void mining(Player sender) {
+		for (int y = 0; y < 7; y++) {
+			for (int x = 0; x < 7; x++) {
+				Hexagon h = game.getGameBoardManager().getBoard().getHexagonAt(x, y);
+				if (h != null) {
+					if (h.getType() == TerrainType.MOUNTAIN) {
+						boolean onHex = false;
+						for (IntersectionLoc loc : IntersectionLoc.values()) {
+							IntersectionUnit u = h.getIntersection(loc).getUnit();
+							if (u instanceof Village) {
+								if (u.getOwner() == sender) {
+									onHex = true;
+									break;
+								}
+							}
+						}
+						if (onHex) {
+							sender.giveResource(ResourceType.ORE, 2);
+							sender.sendCommand(new UpdateResourcesCommand(currentPlayer.getResources()));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get one VP!
+	 */
+	private void printer(Player sender) {
+
+	}
+
+	/**
+	 * Build two roads at no cost
+	 */
+	private void roadBuilding(Player sender) {
+		sender.playRoadBuilding();
+	}
+
+	/**
+	 * Upgrade two knights at no cost
+	 */
+	private void smithCard(Player sender) {
+		sender.playSmith();
 	}
 }
