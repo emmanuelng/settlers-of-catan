135ed123ccd1edae5cde99a51eb53139b2fec5fa
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-04-06 15:47:30 -0400

    Second attempt to fix trade

--- src/catan/settlers/network/server/commands/game/PlayerTradeConfirmCommand.java
+++ src/catan/settlers/network/server/commands/game/PlayerTradeConfirmCommand.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import catan.settlers.client.model.ClientModel;
 import catan.settlers.network.client.commands.game.TradeSuccessCommand;
 import catan.settlers.network.client.commands.game.UpdateResourcesCommand;
+import catan.settlers.network.server.Credentials;
 import catan.settlers.network.server.Server;
 import catan.settlers.network.server.Session;
 import catan.settlers.network.server.commands.ClientToServerCommand;
@@ -30,21 +31,19 @@ public class PlayerTradeConfirmCommand implements ClientToServerCommand {
 	@Override
 	public void execute(Session sender, Server server) {
 		Game game = server.getGameManager().getGameById(gameId);
-		Player player = game.getCurrentPlayer();
+		Player personWhoPropose = game.getCurrentPlayer();
+		Credentials personWhoAcceptsCred = sender.getCredentials();
+		Player personWhoAccepts = game.getPlayersManager().getPlayerByCredentials(personWhoAcceptsCred);
 
-		for (ResourceType s : give.keySet()) {
-			player.giveResource(s, give.get(s));
-			proposedPlayer.removeResource(s, give.get(s));
-		}
+		for (ResourceType rtype : give.keySet())
+			personWhoAccepts.giveResource(rtype, give.get(rtype));
 
-		for (ResourceType t : get.keySet()) {
-			proposedPlayer.giveResource(t, get.get(t));
-			player.removeResource(t, get.get(t));
-		}
+		for (ResourceType rtype : get.keySet())
+			personWhoPropose.removeResource(rtype, get.get(rtype));
 
 		for (Player p : game.getParticipants()) {
 			p.sendCommand(new UpdateResourcesCommand(p.getResources()));
-			p.sendCommand(new TradeSuccessCommand(player.getUsername()));
+			p.sendCommand(new TradeSuccessCommand(personWhoPropose.getUsername()));
 		}
 	}
 
