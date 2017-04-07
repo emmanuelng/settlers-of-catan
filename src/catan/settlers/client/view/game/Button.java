e5411b3f1a500e06f90c5e5da55eced93117510a
Emmanuel Ng <emmanuel_ng@outlook.com>
2017-04-07 14:06:21 -0400

    Improved buttons

--- src/catan/settlers/client/view/game/Button.java
+++ src/catan/settlers/client/view/game/Button.java
@@ -25,8 +25,11 @@ public class Button {
 		MinuetoText textImage = new MinuetoText(text, default_btn_font, textColor);
 		MinuetoRectangle background = new MinuetoRectangle(textImage.getWidth() + 30, textImage.getHeight() + 20, color,
 				true);
+		MinuetoRectangle shadow = new MinuetoRectangle(textImage.getWidth() + 30, textImage.getHeight() + 20, color.darken(0.1),
+				true);
 
-		btnImage = new MinuetoImage(background.getWidth(), background.getHeight());
+		btnImage = new MinuetoImage(background.getWidth() + 3, background.getHeight() + 3);
+		btnImage.draw(shadow, 3, 3);
 		btnImage.draw(background, 0, 0);
 		btnImage.draw(textImage, background.getWidth() / 2 - textImage.getWidth() / 2,
 				background.getHeight() / 2 - textImage.getHeight() / 2);
@@ -48,6 +51,6 @@ public class Button {
 		 * in-white-or-black-depending-on-background-color
 		 */
 		double value = red * 0.299 + green * 0.587 + blue * 0.114;
-		return value > 186 ? MinuetoColor.BLACK : MinuetoColor.WHITE;
+		return value > 186 ? btn_bg_color.darken(0.4) : MinuetoColor.WHITE;
 	}
 }
