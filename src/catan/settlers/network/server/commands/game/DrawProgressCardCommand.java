a4fbb4f660f9fb9357f6061840548fe7733b9523
Kalin <kalin.otto@gmail.com>
2017-04-11 19:28:58 -0400

    oops

---
+++ src/catan/settlers/network/server/commands/game/DrawProgressCardCommand.java
@@ -0,0 +1,31 @@
+package catan.settlers.network.server.commands.game;
+
+import catan.settlers.client.model.ClientModel;
+import catan.settlers.network.server.Server;
+import catan.settlers.network.server.Session;
+import catan.settlers.network.server.commands.ClientToServerCommand;
+import catan.settlers.server.model.Game;
+import catan.settlers.server.model.TurnData;
+import catan.settlers.server.model.TurnData.TurnAction;
+
+public class DrawProgressCardCommand implements ClientToServerCommand {
+
+	private int gameId;
+	private String type;
+	private TurnData data;
+	
+	public DrawProgressCardCommand(String type) {
+		this.gameId = ClientModel.instance.getGameStateManager().getGameId();
+		this.type = type;
+		this.data = new TurnData(TurnAction.DRAW_PROGRESS_CARD);
+		ClientModel.instance.getGameStateManager().setShowSelectCardTypeMenu(false);
+	}
+	
+	@Override
+	public void execute(Session sender, Server server) {
+		Game game = server.getGameManager().getGameById(gameId);
+		data.setSelectedProgressCardType(type);
+		game.receiveResponse(sender.getCredentials(), data);
+	}
+
+}
